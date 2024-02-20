c6e1b4f13260a6e76fa8a69db651c6b556f1d282
shreya-poojari <shreyapoojari553@gmail.com>
2024-02-20 22:07:14 +0530

    java programs

--- DLTE-Java/DITE-java-001/src/main/java/basics/service/Mobilebanking.java
+++ DLTE-Java/DITE-java-001/src/main/java/basics/service/Mobilebanking.java
@@ -2,7 +2,7 @@ package basics.service;
 import java.util.Scanner;
 public class Mobilebanking {
     public static void main(String[] args) {
-        String UserPin = "", destName = "", destAcc = "", destPh = "";
+        String UserPin = "", RecipientName = "", ReciepientAcc = "", ReciepientPh = "";
         Long  amount = 0L, UPIpin = 0L;
         Scanner scanner = new Scanner(System.in);
         System.out.println("--------welcome to MobileBank----------");
@@ -10,11 +10,11 @@ public class Mobilebanking {
         UserPin = scanner.nextLine();
         System.out.println("--------Enter details----------");
         System.out.println("enter name/ account number/ phone number");
-        destName = scanner.nextLine();
+        RecipientName = scanner.nextLine();
         System.out.println("enter amount");
         amount = scanner.nextLong();
         System.out.println("enter upi pin");
         UPIpin = scanner.nextLong();
-        System.out.println("the amount of RS=" + amount + " is transferred to " + destName + " succesfully");
+        System.out.println("the amount of RS=" + amount + " is transferred to " + RecipientName+ " succesfully");
     }
 }
