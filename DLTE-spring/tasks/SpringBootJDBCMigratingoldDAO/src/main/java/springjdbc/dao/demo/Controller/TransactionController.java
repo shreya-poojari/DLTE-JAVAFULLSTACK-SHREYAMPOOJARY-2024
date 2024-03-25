1f64a7d82dd8aa14e90e6b9f97672533b4767761
shreya-poojari <shreyapoojari553@gmail.com>
2024-03-25 12:41:43 +0530

    SpringFramework task

--- DLTE-spring/tasks/SpringBootJDBCMigratingoldDAO/src/main/java/springjdbc/dao/demo/Controller/TransactionController.java
+++ DLTE-spring/tasks/SpringBootJDBCMigratingoldDAO/src/main/java/springjdbc/dao/demo/Controller/TransactionController.java
@@ -12,22 +12,22 @@ import java.util.List;
 public class TransactionController {
     private TransactionService transactionService;
     @PostMapping("/addTransaction")
-    //adding new transaction using http://localhost:1002/Transactions/addTransaction/
+    //http://localhost:1002/Transactions/addTransaction/
     public Transactions newTransaction(@RequestBody Transactions transactions) {
         return transactionService.newTransaction(transactions);
     }
     @GetMapping("/sender/{sender}")
-    //retrieving list of transactions by entering sender's name using http://localhost:1002/Transactions/sender/{sender}
+    //http://localhost:1002/Transactions/sender/{sender}
     public List<Transactions> findBySender(@PathVariable String sender) {
         return transactionService.findBySender(sender);
     }
     @GetMapping("/receiver/{receiver}")
-    //retrieving list of transactions by entering receiver's name using http://localhost:1002/transactions/receiver/{receiver}
+    //http://localhost:1002/transactions/receiver/{receiver}
     public List<Transactions> findByReceiver(@PathVariable String receiver) {
         return transactionService.findByReceiver(receiver);
     }
     @GetMapping("/amount/{amount}")
-    //retrieving list of transactions by entering amount using http://localhost:1002/Transactions/amount/{amount}
+    //http://localhost:1002/Transactions/amount/{amount}
     public List<Transactions> findByAmount(@PathVariable Long amount) {
         return transactionService.findByAmount(amount);
     }
